7778e34c1b1c55d0cd507ee26c97206aae357952
hiroin <egamescape@gmail.com>
2021-03-08 09:51:00 +0100

    フローチャート作成

---
+++ google_test/sample.cc
@@ -0,0 +1,5 @@
+#include "sample.h"
+
+bool IsEven(int x) {
+    return x % 2 == 0;
+}
---
+++ google_test/sample.h
@@ -0,0 +1,9 @@
+#ifndef SAMPLE_H_
+#define SAMPLE_H_
+
+/**
+* 入力値が偶数か判定する関数
+*/
+bool IsEven(int x);
+
+#endif  // SAMPLE_H_
